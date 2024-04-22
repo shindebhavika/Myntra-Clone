15823552bd8f96176b273af34402742808c53b2e
shindebhavika <bhavikashinde74@gmail.com>
2024-04-22 20:35:13 +0530

    initial commit

--- Weather App/src/components/Card.jsx
+++ Weather App/src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 function Card({children }) {
   return (
--- Weather App/src/components/Weather.jsx
+++ Weather App/src/components/Weather.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useState,useEffect } from "react";
+
+import { useState} from "react";
 const api = {
   key: "1e9e95f2cff9503d95fbb0201cfa66ac",
   base: "https://api.openweathermap.org/data/2.5/",
@@ -8,7 +8,7 @@ function Weather() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
-  const search = evt => {
+  const search = (evt) => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
